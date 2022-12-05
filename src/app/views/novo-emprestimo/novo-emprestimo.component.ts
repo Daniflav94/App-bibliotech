161cebfd7ec80af6249369d9ba0f37d6cd3322ba
Daniflav94 <danyelly_mell@hotmail.com>
2022-12-04 21:57:32 -0300

    alterações do livro desfeitas

--- src/app/views/novo-emprestimo/novo-emprestimo.component.ts
+++ src/app/views/novo-emprestimo/novo-emprestimo.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { map, Observable, startWith } from 'rxjs';
 import { Emprestimo } from 'src/app/models/emprestimo';
 import { Livro } from 'src/app/models/livro';
 import { EmprestimosService } from 'src/app/service/emprestimos.service';
@@ -17,9 +16,7 @@ export class NovoEmprestimoComponent implements OnInit {
 
   public formEmprestimo: FormGroup
   public listaLivros: Livro[] = []
-  campoLivro = new FormControl('')
-  livro!: Livro
-  filteredOptions!: Observable<Livro[]>
+  public idLivro: string = ''
 
   constructor(
     fb: FormBuilder,
@@ -32,27 +29,13 @@ export class NovoEmprestimoComponent implements OnInit {
       leitor: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       telefone: ['', [Validators.required]],
-      //livro: ['', [Validators.required]]
+      livro: [{}, [Validators.required]]
     }) 
 
   }  
 
   ngOnInit(): void {
     this.listarLivrosDisponiveis()
-    this.filteredOptions = this.campoLivro.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
-  }
-
-  private _filter(value: string): Livro[] {
-    const filterValue = value.toLowerCase();
-
-    return this.listaLivros.filter(value => value.titulo.toLowerCase().includes(filterValue));
-  }
-
-  livroSelecionado(livro: Livro) {
-    this.livro = livro
   }
 
   public listarLivrosDisponiveis(): void{
@@ -66,8 +49,7 @@ export class NovoEmprestimoComponent implements OnInit {
   public criarEmprestimo(): void{
     if(this.formEmprestimo.valid){
       const emprestimo: Emprestimo = this.formEmprestimo.value
-      emprestimo.livro = this.livro
-      const idlivro = this.livro.id
+      const idLivro = emprestimo.livro.id
       const dataMiliSegundos = Date.now()
       const dataAtual = new Date(dataMiliSegundos)
       emprestimo.dataEmprestimo = dataAtual.toLocaleDateString()
@@ -78,7 +60,7 @@ export class NovoEmprestimoComponent implements OnInit {
           this.router.navigate(['/dashboard'])        
         }
       )
-      this.listaLivrosService.emprestarLivro(idlivro)
+      this.listaLivrosService.emprestarLivro(idLivro)
       
     } else {
       this.notificacao.Showmessage("Verifique os campos preenchidos e tente novamente.")
